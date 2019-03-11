fc974bff74f8c25b00ea290724f30f5f0691bbcf
x1314aq <x1314aq@sina.cn>
2019-03-12 00:08:23 +0800

    re-implement heap using macros

--- include/heap.h
+++ include/heap.h
@@ -1,5 +1,5 @@
 //
-//  Created by x1314aq on 2017/7/24.
+//  Created by x1314aq on 2019/03/08.
 //
 
 #ifndef _LIBTINY_HEAP_H_
@@ -7,30 +7,104 @@
 
 #include "common.h"
 
+#define __lc(i)    (2 * (i) + 1)
+#define __rc(i)    (2 * ((i) + 1))
+#define __pa(i)    (((i) - 1) << 1)
 
-/* 大顶堆 */
-typedef struct{
-    int _size;
-    int _max;
-    void *_cont;
-    int (*_cmp)(const void *, const void *);
-    size_t _ele_size;
-} heap;
+#define HEAP_DECLARE(name, type)    \
+    struct heap_##name {    \
+        size_t num;    \
+        size_t max;    \
+        type *room;    \
+    }
 
-BEGIN_DECL
+#define HEAP_IMPL(name, type, lt_func)    \
+    static inline struct heap_##name *heap_init_##name()    \
+    {    \
+        struct heap_##name *h = malloc(sizeof(struct heap_##name));    \
+        if(!h)    \
+            return NULL;    \
+        h->num = 0;    \
+        h->max = 4;    \
+        h->room = malloc(h->max * sizeof(type));    \
+        if(!h->room) {    \
+            free(h);    \
+            return NULL;    \
+        }    \
+        return h;    \
+    }    \
+    static inline void heap_destroy_##name(struct heap_##name *h)    \
+    {    \
+        free(h->room);    \
+        free(h);    \
+    }    \
+    static inline void heap_percolate_up_##name(struct heap_##name *h, type ele)    \
+    {    \
+        size_t index = h->num++;    \
+        size_t next = __pa(index);    \
+        while(index > 0 && lt_func(h->room[next], ele)) {    \
+            h->room[index] = h->room[next];    \
+            index = next;    \
+            next = __pa(index);    \
+        }    \
+        h->room[index] = ele;    \
+    }    \
+    static inline void heap_percolate_down_##name(struct heap_##name *h, type ele)    \
+    {    \
+        size_t index = 0;    \
+        size_t next = 2;    \
+        while(next < h->num) {    \
+            if(lt_func(h->room[next], h->room[next - 1]))    \
+                next--;    \
+            h->room[index] = h->room[next];    \
+            index = next;    \
+            next = __rc(index);    \
+        }    \
+        if(next == h->num) {    \
+            h->room[index] = h->room[next - 1];    \
+            index = next - 1;    \
+        }    \
+        next = __pa(index);    \
+        while(index > 0 && lt_func(h->room[next], ele)) {    \
+            h->room[index] = h->room[next];    \
+            index = next;    \
+            next = __pa(index);    \
+        }    \
+        h->room[index] = ele;    \
+    }    \
+    static inline int heap_resize_##name(struct heap_##name *h)    \
+    {    \
+        size_t new_max = h->max + h->max >> 1;    \
+        void *temp = realloc(h->room, sizeof(type) * new_max);    \
+        if(!temp)    \
+            return -1;    \
+        h->max = new_max;    \
+        h->room = temp;    \
+        return 0;    \
+    }    \
+    static inline int heap_push_##name(struct heap_##name *h, type ele)    \
+    {    \
+        if(h->num == h->max) {    \
+            if(heap_resize_##name(h))    \
+                return -1;    \
+        }    \
+        heap_percolate_up_##name(h, ele);    \
+        return 0;    \
+    }    \
+    static inline type heap_pop_##name(struct heap_##name *h)    \
+    {    \
+        type temp = h->room[0];    \
+        heap_percolate_down_##name(h, h->room[--h->num]);    \
+        return temp;    \
+    }
 
-heap *heap_init(int maxsz, size_t ele_size, int (*cmp)(const void *, const void *));
-
-heap *heap_heapify(void *base, int size, size_t ele_size, int (*cmp)(const void *, const void *));
-
-void heap_free(heap *hp);
-
-void heap_insert(heap *hp, void *ele);
-
-void *heap_get_max(heap *hp);
-
-void heap_rm_max(heap *hp);
-
-END_DECL
+/**
+ * public API specification
+ */
+#define heap_t(name)             struct heap_##name
+#define heap_init(name)          heap_init_##name()
+#define heap_destroy(name, h)    heap_destroy_##name(h)
+#define heap_push(name, h, ele)  heap_push_##name(h, ele)
+#define heap_pop(name, h)        heap_pop_##name(h)
 
 #endif // _LIBTINY_HEAP_H_
